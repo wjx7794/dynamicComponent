00eb25f7b122d302b2749c57b03c1a1f187d66fb
wjx7794 <wjx7794@163.com>
2024-07-10 08:38:32 +0800

    :sparkles: feat(custom): 测试lint

--- src/Foo/index.tsx
+++ src/Foo/index.tsx
@@ -1,13 +1,13 @@
-import React, { type FC } from 'react';
 import { Button } from 'antd';
+import React, { type FC } from 'react';
 
 const Foo: FC<{ title: string }> = (props) => {
   return (
     <>
-      <h4 style={{color: 'red'}}>{props.title}</h4>
+      <h4 style={{ color: 'red' }}>{props.title}</h4>
       <Button type="primary">Primary Button</Button>
     </>
-  )
+  );
 };
 
 export default Foo;
